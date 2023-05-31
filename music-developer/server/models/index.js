d4dde538aa6294533a0196daa4cf32d694cbfe6c
Michael Mount <mount.photos@gmail.com>
2023-05-31 16:06:08 -0600

    built the index.js for schema

---
+++ music-developer/server/models/index.js
@@ -0,0 +1,15 @@
+const musicUser = require("./musicUser");
+const message = require("./messages");
+const bookings = require("./bookings");
+const musicUserPosts = require("./musicUserPosts");
+const venueUser = require("./venueUser");
+const venueUserPosts = require("./musicUserPosts");
+
+module.exports = {
+  musicUser,
+  message,
+  bookings,
+  musicUserPosts,
+  venueUser,
+  venueUserPosts,
+};
