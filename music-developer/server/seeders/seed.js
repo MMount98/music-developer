9d6bcf19130ff45484a2c4630673a1f3ba729288
Michael Mount <mount.photos@gmail.com>
2023-06-11 11:48:51 -0600

    working on seed.js

---
+++ music-developer/server/seeders/seed.js
@@ -0,0 +1,5 @@
+const db = require("../config/connection")
+const {venueUser, musicUser} = require("../models")
+
+const musicUser = require("./musicUser.json")
+const venueUser = require("./venueUser.json")
