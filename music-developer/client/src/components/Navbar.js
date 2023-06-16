820cbc6b83e392b79d01f83692c96d7ffdc9e3cd
Michael Mount <mount.photos@gmail.com>
2023-06-15 20:02:43 -0600

    building navbar and fixed bug in developmet build commnad

---
+++ music-developer/client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <>
+      <div className="navbar bg-black">
+        <a className="text-white normal-case text-xl">BandBridge</a>
+        <div className="navbar-end">
+          <a className="btn btn-outline rounded text-white">Login</a>
+        </div>
+      </div>
+    </>
+  );
+}
--- music-developer/client/src/utils/queries.js
+++ music-developer/client/src/utils/queries.js
@@ -1,2 +1,24 @@
 import { gql } from "@apollo/client";
 
+export const QUERY_PROFILES = gql`
+  {
+    musicUsers {
+      _id
+      bandCoverPhoto
+      bandName
+      bandProfilePhoto
+      bio
+      genre
+      location
+      socialLinks
+    }
+    venueUsers {
+      _id
+      description
+      location
+      venueName
+      venuePhoto
+      website
+    }
+  }
+`;
