3dd04a1a7db50cea69d0be2052f75d3255c515d7
Michael Mount <mount.photos@gmail.com>
2023-06-28 10:40:42 -0600

    Working on fixing .env keys not passing

--- music-developer/client/src/components/pages/BandsInState.js
+++ music-developer/client/src/components/pages/BandsInState.js
@@ -1,9 +1,8 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import {Link} from 'react-router-dom'
 
-export default function BandsInStatePage()  {
+export default function BandsInStatePage() {
   const [state, setState] = useState(null);
   const [bands, setBands] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -12,21 +11,26 @@ export default function BandsInStatePage()  {
     // Get user's location
     navigator.geolocation.getCurrentPosition(async (position) => {
       const { latitude, longitude } = position.coords;
-      
+
       // Convert coordinates to state using Google's Geocoding API
       try {
-        const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.REACT_APP_GOOGLE_API }`);
+        const response = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.REACT_APP_GOOGLE_API}`
+        );
+        console.log(response.data);
         const addressComponents = response.data.results[0].address_components;
-        const stateObj = addressComponents.find(component => component.types.includes('administrative_area_level_1'));
+        const stateObj = addressComponents.find((component) =>
+          component.types.includes("administrative_area_level_1")
+        );
         const userState = stateObj.long_name;
-        
+
         setState(userState);
 
         // Query your database for bands in the state
         const bandsResponse = await axios.get(`/api/bands?state=${userState}`);
         setBands(bandsResponse.data);
       } catch (error) {
-        console.error('Error getting state:', error);
+        console.error("Error getting state:", error);
       } finally {
         setLoading(false);
       }
@@ -41,7 +45,7 @@ export default function BandsInStatePage()  {
         <div>
           <h1>Bands in {state}</h1>
           <ul>
-            {bands.map(band => (
+            {bands.map((band) => (
               <li key={band.id}>{band.name}</li>
             ))}
           </ul>
@@ -49,4 +53,4 @@ export default function BandsInStatePage()  {
       )}
     </div>
   );
-};
+}
